251424b01fa79f123a933f1e610c36016c837b1e
Angelina <angelinanosova@yandex.ru>
2024-04-25 19:05:49 +0300

    add hash to bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,27 @@
 const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js',
+    filename: 'bundle.[contenthash].js',
     clean: true,
   },
   devtool: 'source-map',
   plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    }),
     new CopyPlugin({
       patterns: [
-        {from: "public"},
+        {
+          from: "public",
+          globOptions: {
+            ignore: ['**/index.html'],
+          },
+        },
       ],
     }),
   ],
