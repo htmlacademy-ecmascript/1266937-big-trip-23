f21424c52c660b98c1e0d5e04f2285fc0d8bd239
Angelina <angelinanosova@yandex.ru>
2024-04-25 17:40:09 +0300

    setup Babel

--- webpack.config.js
+++ webpack.config.js
@@ -16,5 +16,20 @@ module.exports = {
       ],
     }),
   ],
-
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env']
+            ]
+          }
+        }
+      }
+    ]
+  }
 };
