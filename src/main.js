b5e07f36d0cede0270c2db25d3697605f9ea867d
Angelina <angelinanosova@yandex.ru>
2024-05-02 21:15:52 +0300

    create component to render sorting form

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-import {render} from './render';
-import FilterView from './view/filter-view';
+import {render} from './render.js';
+import FilterView from './view/filter-view.js';
+import SortingView from './view/sorting-view.js';
 
 const filterControlsElement = document.querySelector('.trip-controls__filters');
+const tripEventsElement = document.querySelector('.trip-events');
 
 render(new FilterView(), filterControlsElement);
+render(new SortingView(), tripEventsElement);
