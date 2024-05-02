1d88cbb26f8f1444f44a8e7a1e32bdcc6b7a8f34
Angelina <angelinanosova@yandex.ru>
2024-05-02 23:12:02 +0300

    finish header rendering

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
-import {render} from './render.js';
+import {RenderPosition, render} from './render.js';
 import FilterView from './view/filter-view.js';
 import SortingView from './view/sorting-view.js';
+import TripInfoView from './view/trip-info-view.js';
 
+const tripInfoElement = document.querySelector('.trip-main');
 const filterControlsElement = document.querySelector('.trip-controls__filters');
 const tripEventsElement = document.querySelector('.trip-events');
 
+render(new TripInfoView(), tripInfoElement, RenderPosition.AFTERBEGIN);
 render(new FilterView(), filterControlsElement);
 render(new SortingView(), tripEventsElement);
