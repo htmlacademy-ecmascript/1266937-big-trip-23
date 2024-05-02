1f6997794412b835afd2d362527680cc4dcebaf4
Angelina <angelinanosova@yandex.ru>
2024-05-03 02:02:20 +0300

    add components for rendering trip points

--- src/main.js
+++ src/main.js
@@ -1,12 +1,23 @@
 import {RenderPosition, render} from './render.js';
+import TripInfoView from './view/trip-info-view.js';
 import FilterView from './view/filter-view.js';
 import SortingView from './view/sorting-view.js';
-import TripInfoView from './view/trip-info-view.js';
+import PointListView from './view/point-list-view.js';
+import PointView from './view/point-view.js';
 
-const tripInfoElement = document.querySelector('.trip-main');
+const tripMainElement = document.querySelector('.trip-main');
 const filterControlsElement = document.querySelector('.trip-controls__filters');
 const tripEventsElement = document.querySelector('.trip-events');
 
-render(new TripInfoView(), tripInfoElement, RenderPosition.AFTERBEGIN);
+render(new TripInfoView(), tripMainElement, RenderPosition.AFTERBEGIN);
 render(new FilterView(), filterControlsElement);
 render(new SortingView(), tripEventsElement);
+
+const renderPointList = (pointListContainer) => {
+  const pointListComponent = new PointListView();
+
+  render(pointListComponent, pointListContainer);
+  render(new PointView(), pointListComponent.getElement());
+};
+
+renderPointList(tripEventsElement);
